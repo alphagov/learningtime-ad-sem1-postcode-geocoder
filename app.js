1faebceb4071c63fb477e9a9a780e14c63ecf390
antoni-devlin <antoni.devlin@gmail.com>
2023-05-05 15:53:55 +0100

    Remove hardcoded postcode

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ function getPostcodeFromArgs() {
   );
 }
 
-let hardcodedPostcode = "se13 7gp"; // Postcode hardcoded for the moment
-
 async function geocodePostcode(postcode) {
   let response = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
   return await response.json();
