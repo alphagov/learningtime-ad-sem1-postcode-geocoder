281f1c8b804945daa4015b3b4489e1da37bc2602
antoni-devlin <antoni.devlin@gmail.com>
2023-05-05 15:59:26 +0100

    Added error handling for when too many arguments are given on the command line.

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ let base_endpoint = "https://api.postcodes.io/postcodes/";
 
 function getPostcodeFromArgs() {
   let postcode = process.argv[2];
+  if (process.argv.length > 3) {
+    throw new Error(
+      "Too many arguments. This tool expects a single UK postcode argument to be given on the command line. Your postcode should not contain any spaces.`"
+    );
+  }
   if (postcode) {
     return String(postcode);
   }
   throw new Error(
-    "No postcode given. You can provide a postcode by running `node app.js <postcode>. Your postcode should not contain spaces.`"
+    "No postcode given. You can provide a UK postcode by running `node app.js <postcode>. Your postcode should not contain spaces.`"
   );
 }
 
