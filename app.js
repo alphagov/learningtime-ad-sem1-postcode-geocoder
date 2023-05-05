de1babe39f94b18dd7298c2a0c2a8fbd5bcea00b
antoni-devlin <antoni.devlin@gmail.com>
2023-05-05 15:05:12 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let base_endpoint = "https://api.postcodes.io/postcodes/";
+
+function isPostcode(postcode) {
+  return false;
+}
+
+// async function geocodePostcode(postcode) {
+//   let response = await fetch("https://api.postcodes.io/postcodes/se146pw");
+//   return await response.json();
+// }
+
+// let coords = await geocodePostcode();
