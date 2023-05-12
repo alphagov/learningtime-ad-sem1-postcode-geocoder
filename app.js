c5aac096a07e14a61f4f3c165798e5978e7e5923
antoni-devlin <antoni.devlin@gmail.com>
2023-05-12 14:32:59 +0100

    Moved getPostcodeFromArgs from app.js to cli.js This is to better separate the cli and postcode logic. Decoupling the code that deals with the cli from the code that does all the postcode lookups and validation will make it easier to adapt the backend to work with other interfaces than a cli in the future (e.g. a REST API or web frontend).

--- app.js
+++ app.js
@@ -11,21 +11,6 @@ export async function validatePostcode(postcode) {
   return response["result"];
 }
 
-export function getPostcodeFromArgs() {
-  let postcode = process.argv[2];
-  if (process.argv.length > 3) {
-    throw new Error(
-      "Too many arguments. This tool expects a single UK postcode argument to be given on the command line. Your postcode should not contain any spaces.`"
-    );
-  }
-  if (postcode) {
-    return String(postcode);
-  }
-  throw new Error(
-    "No postcode given. You can provide a UK postcode by running `node app.js <postcode>. Your postcode should not contain spaces.`"
-  );
-}
-
 export async function geocodePostcode(postcode) {
   const response = await fetch(
     `https://api.postcodes.io/postcodes/${postcode}`
@@ -39,8 +24,8 @@ export function getCoordinates(rawJSON) {
   return [latitude, longitude];
 }
 
-export async function main() {
-  const postcode = getPostcodeFromArgs();
+export async function main(postcodeFromArgs) {
+  const postcode = postcodeFromArgs;
   const postcodeIsValid = await validatePostcode(postcode);
   if (postcodeIsValid) {
     const coordinates = await geocodePostcode(postcode);
--- cli.js
+++ cli.js
@@ -2,7 +2,22 @@ import { main, log } from "./app.js";
 
 const base_maps_url = "http://maps.google.com/maps?z=12&t=m&q=loc:";
 
-const coordinates = await main();
+export function getPostcodeFromArgs() {
+  let postcode = process.argv[2];
+  if (process.argv.length > 3) {
+    throw new Error(
+      "Too many arguments. This tool expects a single UK postcode argument to be given on the command line. Your postcode should not contain any spaces.`"
+    );
+  }
+  if (postcode) {
+    return String(postcode);
+  }
+  throw new Error(
+    "No postcode given. You can provide a UK postcode by running `node app.js <postcode>. Your postcode should not contain spaces.`"
+  );
+}
+
+const coordinates = await main(getPostcodeFromArgs());
 
 log(`Coordinates: ${coordinates[0]},${coordinates[1]}`);
 log(`Google maps link: ${base_maps_url}${coordinates[0]}+${coordinates[1]}}`);
