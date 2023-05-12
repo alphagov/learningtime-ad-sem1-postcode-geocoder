62930107aab010d4aa10dd4c1ba43cc2c49dad5d
antoni-devlin <antoni.devlin@gmail.com>
2023-05-12 08:52:22 +0100

    Wrote tests for CLI argument parsing function

--- app.test.js
+++ app.test.js
@@ -19,27 +19,41 @@ import {
   getPostcodeFromArgs,
 } from ".";
 
-process.argv = ["node", "app.js"];
-
 describe("Argument parsing", () => {
-  test("npm app.js se137gp", () => {
+  test("node app.js se137gp", () => {
+    // Set up virtual command prompt
+    process.argv = ["node", "app.js"];
     process.argv.push("se137gp");
+
+    const result = getPostcodeFromArgs();
+    expect(result).toBe("se137gp");
+  });
+
+  test("node app.js se137gp", () => {
+    // Set up virtual command prompt
+    process.argv = ["node", "app.js"];
+    process.argv.push("se137gp");
+
     const result = getPostcodeFromArgs();
     expect(result).toBe("se137gp");
   });
 
   test("throws an error if more than 3 args", () => {
+    // Set up virtual command prompt
+    process.argv = ["node", "app.js"];
     process.argv.push("se13");
     process.argv.push("7gp");
+
+    expect(() => {
+      getPostcodeFromArgs();
+    }).toThrow("Too many arguments.");
+  });
+
+  test("throws an error if no postcode arg given", () => {
+    // Set up virtual command prompt
+    process.argv = ["node", "app.js"];
     expect(() => {
       getPostcodeFromArgs();
     }).toThrow();
   });
-
-  ////   Can't get this to work yet. It should throw if there are only two args on the command line (i.e. if process.argv.length < 3)
-  //   test("throws an error if no postcode arg given", () => {
-  //     expect(() => {
-  //       getPostcodeFromArgs();
-  //     }).toThrow();
-  //   });
 });
