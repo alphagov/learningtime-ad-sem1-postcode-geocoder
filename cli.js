983632d053b6746244f7f100e611be279e20684a
antoni-devlin <antoni.devlin@gmail.com>
2023-05-12 13:42:23 +0100

    Logged coordinate string and built google maps url from cli.js.

--- cli.js
+++ cli.js
@@ -1,3 +1,8 @@
 import { main, log } from "./app.js";
 
-console.log(await main());
+let base_maps_url = "http://maps.google.com/maps?z=12&t=m&q=loc:";
+
+let coordinates = await main();
+
+log(`Coordinates: ${coordinates[0]},${coordinates[1]}`);
+log(`Google maps link: ${base_maps_url}${coordinates[0]}+${coordinates[1]}}`);
