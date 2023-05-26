4ec1b1afd74a0571b1caaedb9a0febcf7418287c
antoni-devlin <antoni.devlin@gmail.com>
2023-05-26 09:13:11 +0100

    Moved type definitions from separate files in the src/types folder, directly into postcodes.ts. This stops the types folder and garbage .ts files also being sent to /dist by the compiler.

--- src/postcodes.ts
+++ src/postcodes.ts
@@ -1,5 +1,51 @@
-import { PostcodeResponse } from "./types/PostcodeResponse";
-import { Coordinates } from "./types/Coordinates";
+// Type definitions
+interface Coordinates extends Array<number> {}
+
+interface PostcodeResponse {
+  status: number;
+  result: {
+    postcode: string;
+    quality: number;
+    eastings: number;
+    northings: number;
+    country: string;
+    nhs_ha: string;
+    longitude: number;
+    latitude: number;
+    european_electoral_region: string;
+    primary_care_trust: string;
+    region: string;
+    lsoa: string;
+    msoa: string;
+    incode: string;
+    outcode: string;
+    parliamentary_constituency: string;
+    admin_district: string;
+    parish: string;
+    admin_county: null | any;
+    date_of_introduction: number;
+    admin_ward: string;
+    ced: null | any;
+    ccg: string;
+    nuts: string;
+    pfa: string;
+    codes: {
+      admin_district: string;
+      admin_county: string;
+      admin_ward: string;
+      parish: string;
+      parliamentary_constituency: string;
+      ccg: string;
+      ccg_id: string;
+      ced: string;
+      nuts: string;
+      lsoa: string;
+      msoa: string;
+      lau2: string;
+      pfa: string;
+    };
+  };
+}
 
 const base_endpoint = "https://api.postcodes.io/postcodes/";
 
