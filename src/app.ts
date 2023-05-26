0c5282dd0b8384c05b7e7b8c1e824bc1de473f32
antoni-devlin <antoni.devlin@gmail.com>
2023-05-26 15:05:12 +0100

    Imported postcode validation/geocoding/formatting functions and used them to create a /postcode/ get route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import {log, validatePostcode, geocodePostcode, getCoordinates} from "./postcodes.js";
+const base_maps_url = "http://maps.google.com/maps?z=12&t=m&q=loc:";
+
 const app = express();
 
 app.get("/", (req, res) => {
 	return res.send("GET request to '/'");
+});
+
+app.get("/postcode/:givenPostcode", async function(req, res, next){
+	const postcode = req.params.givenPostcode;
+	if (await validatePostcode(postcode)) {
+		const postcodeioResponse = await geocodePostcode(postcode);
+		const coords = getCoordinates(postcodeioResponse);
+		const response = [{location:`${base_maps_url}${coords[0]}+${coords[1]}`},{coordinates:coords}];
+		return res.send(response);
+	}
+	return res.status(500).send({ error: "Not a valid postcode" });
 });
 
 app.listen("3000", () => log("Listening on port 3000"));
