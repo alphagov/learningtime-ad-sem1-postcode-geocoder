9382e9d9b52fc9f34ed3ea6d2889350874b3c4c5
antoni-devlin <antoni.devlin@gmail.com>
2023-06-02 11:45:58 +0100

    Added function to call the geocode-postcode api

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,8 @@
+async function makeRequest(postcode) {
+	const response = await fetch(`https://postcode-geocoder.onrender.com/postcode/${postcode}`);
+	const jsonResponse = await response.json();
+	const coordinates = jsonResponse[1]["coordinates"];
+	return coordinates;
+}
 
 
