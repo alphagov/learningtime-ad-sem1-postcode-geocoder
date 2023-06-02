cc5ffba0f0ef5f0c95ad21517ea59f73f2b948ab
antoni-devlin <antoni.devlin@gmail.com>
2023-06-02 11:46:52 +0100

    Added logic for passing postcode input by user to the makeRequest() function using an event listener.

--- frontend/script.js
+++ frontend/script.js
@@ -5,4 +5,19 @@ async function makeRequest(postcode) {
 	return coordinates;
 }
 
+// Setup
+const form = document.getElementById("postcode-form");
+const result = document.getElementById("coordinates");
 
+
+// Form submission Eventlistener
+// TODO: #42 This should be rate limited somehow. Clicking it over and over sends a request everytime.
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	let postcodeValue = document.getElementById("postcode-field").value.trim();
+	postcodeValue = postcodeValue.replace(/\s+/g, "");
+	document.getElementById("postcode-field").value = "";
+	// TODO: #44 Add a loader while waiting for the fetch call to complete
+	result.innerHTML = await makeRequest(postcodeValue);
+	// TODO: #43 Catch the "Not a valid postcode" error returned by the server
+});
