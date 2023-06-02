1dd0df39f7df0fa9bba9fe812de965832518a27a
antoni-devlin <antoni.devlin@gmail.com>
2023-06-02 13:37:52 +0100

    Added error handling when api response is "invalid postcode"

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,10 @@
 async function makeRequest(postcode) {
 	const response = await fetch(`https://postcode-geocoder.onrender.com/postcode/${postcode}`);
 	const jsonResponse = await response.json();
+	if ("error" in jsonResponse) {
+		const errormsg = jsonResponse["error"];
+		return errormsg;
+	}
 	const coordinates = jsonResponse[1]["coordinates"];
 	return coordinates;
 }
