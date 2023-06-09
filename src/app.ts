40c024fc163dd4eb8d4ae2c98347f47487614697
antoni-devlin <antoni.devlin@gmail.com>
2023-06-09 09:55:45 +0100

    Updated tabs to 2 spaces as per the GDS way

--- src/app.ts
+++ src/app.ts
@@ -6,18 +6,18 @@ const base_maps_url = "http://maps.google.com/maps?z=12&t=m&q=loc:";
 const app = express();
 
 app.get("/", (req, res) => {
-	return res.send("GET request to '/'");
+  return res.send("GET request to '/'");
 });
 
 app.get("/postcode/:givenPostcode", cors(), async function(req, res){
-	const postcode = req.params.givenPostcode;
-	if (await validatePostcode(postcode)) {
-		const postcodeioResponse = await geocodePostcode(postcode);
-		const coords = getCoordinates(postcodeioResponse);
-		const response = [{location:`${base_maps_url}${coords[0]}+${coords[1]}`},{coordinates:coords}];
-		return res.send(response);
-	}
-	return res.status(500).send({ error: "Not a valid postcode" });
+  const postcode = req.params.givenPostcode;
+  if (await validatePostcode(postcode)) {
+    const postcodeioResponse = await geocodePostcode(postcode);
+    const coords = getCoordinates(postcodeioResponse);
+    const response = [{location:`${base_maps_url}${coords[0]}+${coords[1]}`},{coordinates:coords}];
+    return res.send(response);
+  }
+  return res.status(500).send({ error: "Not a valid postcode" });
 });
 
 app.listen("3000", () => log("Listening on port 3000"));
--- src/postcodes.ts
+++ src/postcodes.ts
@@ -51,24 +51,24 @@ const base_endpoint = "https://api.postcodes.io/postcodes/";
 
 // Quicker logging helper function
 export function log(msg: any) {
-	console.log(msg);
+  console.log(msg);
 }
 
 export async function validatePostcode(postcode: string) {
-	const rawresponse = await fetch(`${base_endpoint}${postcode}/validate`);
-	const response = await rawresponse.json();
-	return response["result"];
+  const rawresponse = await fetch(`${base_endpoint}${postcode}/validate`);
+  const response = await rawresponse.json();
+  return response["result"];
 }
 
 export async function geocodePostcode(postcode: string) {
-	const response = await fetch(
-		`https://api.postcodes.io/postcodes/${postcode}`
-	);
-	return await response.json();
+  const response = await fetch(
+    `https://api.postcodes.io/postcodes/${postcode}`
+  );
+  return await response.json();
 }
 
 export function getCoordinates(rawJSON: PostcodeResponse): Coordinates {
-	const longitude = rawJSON["result"]["longitude"];
-	const latitude = rawJSON["result"]["latitude"];
-	return [latitude, longitude];
+  const longitude = rawJSON["result"]["longitude"];
+  const latitude = rawJSON["result"]["latitude"];
+  return [latitude, longitude];
 }
